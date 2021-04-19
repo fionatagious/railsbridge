4b9fb18d86f6ac23a2d7203d5585412637c575dc
ftang-cfa <ftang@codeforamerica.org>
2021-04-18 21:08:24 -0700

    add upvote button, add a new route for voting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :topics
+  resources :topics do
+    member do # add a new route for voting
+      post 'upvote'
+    end
+  end 
   root 'topics#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
